2998d14298cb3d1f193194eda5d63cb8fd1f8507
okcoder <okcoder@live.com>
2023-12-10 12:27:45 +0900

    create project

---
+++ src/main/java/org/okcoder/sample/spring_programmatically_load_yaml/SpringProgrammaticallyLoadYamlApplication.java
@@ -0,0 +1,13 @@
+package org.okcoder.sample.spring_programmatically_load_yaml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProgrammaticallyLoadYamlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProgrammaticallyLoadYamlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/okcoder/sample/spring_programmatically_load_yaml/SpringProgrammaticallyLoadYamlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.okcoder.sample.spring_programmatically_load_yaml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProgrammaticallyLoadYamlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
